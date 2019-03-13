d753c8e06326f62fa980a0f21b57f609f360e9b3
Mike <mroyski@gmail.com>
2019-03-13 13:37:43 -0400

    fixed navbar

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -11,8 +19,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName(".mySlides");
-  var dots = document.getElementsByClassName(".dot");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
