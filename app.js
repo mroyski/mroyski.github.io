098149ff11e300692b63e06a78e96ee25b87129e
Mike <mroyski@gmail.com>
2019-03-15 12:53:54 -0400

    changed button to icon

--- app.js
+++ app.js
@@ -42,6 +42,27 @@ for (x = 0; x < coll.length; x++) {
   });
 }
 
+// MODAL BOX
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // NAVBAR
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
